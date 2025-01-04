e0b6a11b4234ef21c59a1d9acc9e481d3e6127f2
pexomat <j.konigsmark@gmail.com>
2025-01-04 15:27:52 +0100

    Add ChildrenFC

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,9 @@
-import { ReactNode } from 'react';
-
 import { Header } from './Header';
 import { Footer } from './Footer';
+import { ChildrenFC } from '../../utils/type';
 import style from './Layout.module.scss';
 
-export const Layout: React.FC<{ children: ReactNode }> = ({ children }) => (
+export const Layout: ChildrenFC = ({ children }) => (
   <>
     <Header />
     <main className={style.content}>{children}</main>
